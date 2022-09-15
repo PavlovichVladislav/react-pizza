c61cefd93f94d4f32da2a62efe175d157a90f6e5
jeffry2814@gmail.com <vplaa@mail.ru>
2022-09-15 20:42:07 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from "./components/categories/Categories";
+import Header from "./components/header/Header";
+import PizzaCard from "./components/pizzaCard/pizzaCard.";
+import Sort from "./components/sort/Sort";
+import "./scss/app.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaCard title="Чизбургер-пицца" price="395" />
+              <PizzaCard title="Мексиканская" price="500" />
+              <PizzaCard title="Мексиканская" price="500" />
+              <PizzaCard title="Мексиканская" price="500" />
+              <PizzaCard title="Мексиканская" price="500" />
+              <PizzaCard title="Мексиканская" price="500" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
