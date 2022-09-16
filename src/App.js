7e40b331f2343b4dae0957852bf0ed886562ab0c
jeffry2814@gmail.com <vplaa@mail.ru>
2022-09-16 13:29:15 +0700

    render list's from json file

--- src/App.js
+++ src/App.js
@@ -4,31 +4,32 @@ import PizzaCard from "./components/pizzaCard/pizzaCard.";
 import Sort from "./components/sort/Sort";
 import "./scss/app.scss";
 
+import pizzas from "./assets/pizza.json";
+
 function App() {
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
+   const renderPizzasBlock = (pizzas) => {
+      return pizzas.map((pizza) => <PizzaCard key={pizza.id} {...pizza} />);
+   };
+
+   return (
+      <div className="App">
+         <div className="wrapper">
+            <Header />
+            <div className="content">
+               <div className="container">
+                  <div className="content__top">
+                     <Categories />
+                     <Sort />
+                  </div>
+                  <h2 className="content__title">Все пиццы</h2>
+                  <div className="content__items">
+                     {renderPizzasBlock(pizzas)}
+                  </div>
+               </div>
             </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              <PizzaCard title="Чизбургер-пицца" price="395" />
-              <PizzaCard title="Мексиканская" price="500" />
-              <PizzaCard title="Мексиканская" price="500" />
-              <PizzaCard title="Мексиканская" price="500" />
-              <PizzaCard title="Мексиканская" price="500" />
-              <PizzaCard title="Мексиканская" price="500" />
-            </div>
-          </div>
-        </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
+
 const Categories = () => {
-  return (
-    <div className="categories">
-      <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
-      </ul>
-    </div>
-  );
+   const [activeCategory, setActiveCategory] = useState("Все");
+
+   const categoriesArr = [
+      "Все",
+      "Мясные",
+      "Вегетарианская",
+      "Гриль",
+      "Острые",
+      "Закрытые",
+   ];
+
+   return (
+      <div className="categories">
+         <ul>
+            {categoriesArr.map((category, i) => (
+               <li
+                  className={category === activeCategory ? "active" : ""}
+                  onClick={() => setActiveCategory(category)}
+                  key={i}
+               >
+                  {category}
+               </li>
+            ))}
+         </ul>
+      </div>
+   );
 };
 
 export default Categories;
